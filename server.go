13fb0b60216a1294ed83cdbe6801d5eb2a91dc03
Anthony Peterson <anthony@petersonrace.com>
2020-05-27 21:33:00 +0000

    Use echo templating and load canvas template

--- server.go
+++ server.go
@@ -1,32 +1,54 @@
 package main
 
 import (
+  "io"
+  "html/template"
 	"net/http"
+  //"database/sql"
 	
+  //_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
   "github.com/labstack/echo/middleware"
   
 )
 
+//Setting up echo templates
+type Template struct {
+  templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+  return t.templates.ExecuteTemplate(w, name, data)
+}
+
+
 func main() {
-// Echo Server
+// Load templates
+  t := &Template{
+    templates: template.Must(template.ParseGlob("html/*.html")),
+  }
+
+// Start echo Server
 	e := echo.New()
 
+// Render templates
+  e.Renderer = t
+ 
 // Middleware Setup
-   e.Use(middleware.Recover())
-	 e.Use(middleware.Logger())
-// HTTPS Redirect
+  e.Use(middleware.Recover())
+  e.Use(middleware.Logger())
+
+// HTTP to HTTPS Redirect
   e.Pre(middleware.HTTPSRedirect())
   
+// Load statics at root
+  e.Static("/", "html")
+  
 // Routing
- 
- e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-			<h1>Welcome to Echo!</h1>
-			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
-		`)
+  e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", template.HTML("<p>HTML Test</p>"))
 	})
- 
- 
- e.Logger.Fatal(e.StartTLS(":443", "cert.pem", "key.pem"))
+
+// Start server
+  e.Logger.Fatal(e.StartTLS(":443", "cert.pem", "key.pem"))
 }
